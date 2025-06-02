ab805599f9873738f20e27f7347959c05950398c
EHAB3x <ehabmah6969@gmail.com>
2025-06-03 00:58:51 +0300

    Add logic to create 404.html from index.html in deploy script

--- post-commit-deploy.ps1
+++ post-commit-deploy.ps1
@@ -1,4 +1,3 @@
-
 # Set paths
 $projectRoot = "D:\web\Angular\portfolio-dashboard"
 $deployPath = "D:\web\Angular\deploy"
@@ -23,6 +22,16 @@ Get-ChildItem -Path $deployPath -Force | Where-Object { $_.Name -ne ".git" } | R
 Copy-Item -Path $buildOutput\* -Destination $deployPath -Recurse
 
 Write-Host "Deploy folder updated with latest build."
+
+# Create 404.html with the same content as index.html in deploy folder
+$indexFile = Join-Path $deployPath "index.html"
+$notFoundFile = Join-Path $deployPath "404.html"
+if (Test-Path $indexFile) {
+    Copy-Item -Path $indexFile -Destination $notFoundFile -Force
+    Write-Host "404.html created from index.html."
+} else {
+    Write-Host "index.html not found in deploy folder. 404.html not created."
+}
 
 # Prompt for commit message
 $commitMessage = Read-Host "Enter commit message for deploy folder"
